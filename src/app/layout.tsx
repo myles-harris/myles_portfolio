387d26593eabb575a8b2b99f36d9dc51dc2c57d4
Myles Harris <mylesharris@Myless-MBP.lan>
2025-06-06 15:48:26 -0700

    Add Vercel Analytics for web analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import BackgroundVideo from "../components/BackgroundVideo";
 import MobileWarningDialog from "../components/MobileWarningDialog";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
         <Navbar />
         <MobileWarningDialog />
         {children}
+        <Analytics />
       </body>
     </html>
   );
